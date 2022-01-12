c89b452ef4927569afe88397727b2e703772ea15
Alec Chen <alecchendev@gmail.com>
2022-01-12 15:43:36 -0600

    Implement ExchangeBooth struct

--- program/src/state.rs
+++ program/src/state.rs
@@ -5,4 +5,9 @@ use std::mem::size_of;
 #[derive(BorshSerialize, BorshDeserialize, Debug, Clone)]
 pub struct ExchangeBooth {
     // TODO
+    admin: Pubkey,
+    oracle: Pubkey,
+    token_account1: Pubkey,
+    token_account2: Pubkey,
+    fee: u8,
 }
